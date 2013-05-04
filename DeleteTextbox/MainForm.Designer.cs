7c6fef0687ec4187ee5d5de925dbbcb25e114346
surfmuggle <gabriel@pixelpiz.de>
2013-05-04 13:22:19 +0200

    First Commit to answer how to Automatically delete textbox
    Automatically delete textbox if textbox.text = empty on focus leave
    http://stackoverflow.com/questions/16345549

---
+++ DeleteTextbox/MainForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Abe
+ * Date: 04.05.2013
+ * Time: 12:50
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace DeleteTextbox
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnSave = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// btnSave
+			// 
+			this.btnSave.Location = new System.Drawing.Point(340, 295);
+			this.btnSave.Name = "btnSave";
+			this.btnSave.Size = new System.Drawing.Size(75, 23);
+			this.btnSave.TabIndex = 0;
+			this.btnSave.Text = "Save";
+			this.btnSave.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(30, 40);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "label1";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(240, 40);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(171, 22);
+			this.textBox1.TabIndex = 2;
+			this.textBox1.Text = "t";
+			this.textBox1.Leave += new System.EventHandler(this.TextBox_LeaveEvent);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(709, 486);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.btnSave);
+			this.Name = "MainForm";
+			this.Text = "DeleteTextbox";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btnSave;
+	}
+}
---
+++ DeleteTextbox/MainForm.cs
@@ -0,0 +1,89 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Abe
+ * Date: 04.05.2013
+ * Time: 12:50
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace DeleteTextbox
+{
+	/// <summary>
+	/// Automatically delete textbox and label if textbox.text = empty on focus leave
+	/// http://stackoverflow.com/questions/16345549
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		
+		List<TextBox> textboxList = new List<TextBox>();
+		
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		
+		void TextBox_LeaveEvent(object sender, EventArgs e)
+		{
+			var tb = sender as TextBox;		
+			// add another textbox if this tb has text			
+			if(textboxList.Count<5 && tb.Text.Length>0){
+				var newTextBox = getNewTextBox(textboxList.Count);			
+				textboxList.Add(newTextBox);
+			} // remove textbox if it has no text
+			else if(tb.Text.Length == 0){
+				RemoveTextBox(tb);
+			}						
+		}
+		
+		
+		TextBox getNewTextBox(int i)
+		{
+			var tb = new TextBox();
+			tb.Location = new System.Drawing.Point(220, 90 + i * 24);
+			tb.Name = "tb_" + i.ToString();
+			tb.Size = new System.Drawing.Size(80,20);
+			tb.TabIndex = i + 2;
+			btnSave.TabIndex = i + 3;
+			tb.Text = i.ToString(); //String.Empty;						
+			tb.Leave += new System.EventHandler(this.TextBox_LeaveEvent);
+			this.Controls.Add(tb);
+			this.Refresh();
+			return tb;
+		}
+		
+		void RemoveTextBox(TextBox tb){								
+				// this.Controls.RemoveByKey(tb.Name);				
+				int tbIndex = this.Controls.IndexOf(tb);
+				this.Controls[tbIndex].Dispose();
+				textboxList.Remove(tb);			
+		}
+		
+		
+		void Button_Enter(object sender, EventArgs e)
+		{
+			// RemoveEmptyTextBox
+			// get last Textbox in List
+			var tb = textboxList[textboxList.Count-1];
+			label1.Text = "Last Textbox in List: " + tb.Name + " (" + tb.Text.Length +")";
+			if(tb.Text.Length==0){
+				// this.Controls.RemoveByKey(tb.Name);				
+				int tbIndex = this.Controls.IndexOf(tb);
+				this.Controls[tbIndex].Dispose();
+				textboxList.Remove(tb);
+			}
+		}		
+	}
+}
