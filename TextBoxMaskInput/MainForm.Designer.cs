a70273e034bac00280bba14821f5e41cc9e38d70
surfmuggle <gabriel@pixelpiz.de>
2013-05-04 15:48:25 +0200

    Readme for repository added

---
+++ TextBoxMaskInput/MainForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Abe
+ * Date: 04.05.2013
+ * Time: 14:36
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace TextBoxMaskInput
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(60, 33);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(142, 22);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Textbox_KeyPressEvent);
+			this.textBox1.Leave += new System.EventHandler(this.TextBox_LeaveEvent);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(127, 77);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(60, 125);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "label1";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(406, 308);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "MainForm";
+			this.Text = "TextBoxMaskInput";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+	}
+}
---
+++ TextBoxMaskInput/MainForm.cs
@@ -0,0 +1,74 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Abe
+ * Date: 04.05.2013
+ * Time: 14:36
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TextBoxMaskInput
+{
+	/// <summary>	
+	/// Dynamic mask textbox money Input
+	/// http://stackoverflow.com/questions/15989717/
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+						
+		}
+		
+		//this only allow numbers and "." and "," on textimbox imput
+	    private void Textbox_KeyPressEvent(object sender, KeyPressEventArgs e)
+	    {
+	    	// TODO: difference in cultures regarding decimal seperator "." vs. ","
+	    	// 		us 1,000.20 onethousand and 20 cent 
+	    	// 		eu 1.000,20 onethousand and 20 cent
+	    	var keyChar = e.KeyChar;
+	    	var tbText = (sender as TextBox).Text;
+	        if (!char.IsControl(keyChar)
+	    		&& !char.IsDigit(keyChar)
+	    		&& keyChar != '.' && keyChar != ',')     		
+	        {
+	            e.Handled = true;
+	        }
+	
+	        // only allow one decimal point
+	        if (keyChar == '.' && tbText.IndexOf('.') > -1)
+	        {
+	            e.Handled = true;
+	        }          
+	        if(keyChar == ',' && tbText.IndexOf(',') > -1){
+	           e.Handled = true;
+	        }
+	    }// Textbox_KeyPressEvent
+	    
+	    private void TextBox_LeaveEvent(object sender, EventArgs e)
+	    {
+	    	var tb = sender as TextBox;
+	    	var tbText = tb.Text;
+	    	if(tbText.IndexOf(" ") !=-1){
+	    		var tbText2 = tbText.Substring(0, tbText.IndexOf(" ")-1);	    	
+	    		var temp = String.Format("{0:D0}", tbText2);
+				MessageBox.Show("temp: _" + temp + "_");
+	        	
+	    	}
+	    	if(tbText.Length>0 && tbText.IndexOf(" ") == -1){
+	    		decimal cubic = Convert.ToDecimal(tbText); 
+	        	tb.Text = string.Format("{0:c}", Convert.ToDecimal(cubic));
+	        	label1.Text = tb.Text;
+	    	}
+	    }
+    
+	}// end of class
+}
